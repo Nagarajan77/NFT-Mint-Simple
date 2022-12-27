3039728a7d3c5d6d07228fea1d4ba0bdaf5e0693
Nagarajan <71954561+Nagarajan77@users.noreply.github.com>
2022-12-27 11:47:50 +0530

    Simple Mint

---
+++ NFTMintSimple.sol
@@ -0,0 +1,41 @@
+// SPDX-Licence-idintifier:MIT 
+pragma solidity ^0.8.0;
+
+
+    import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+    import '@openzeppelin/contracts/access/Ownable.sol';
+
+contract Mintnft is ERC721, Ownable {
+   
+    uint256 public mintprice = 0.05 ether;
+    uint256 public totalsupply;
+    uint256 public MaxSupply;
+    bool public isMintEnable;
+    mapping (address => uint256) public mintedWallets;
+
+    constructor () payable ERC721('Mintnft','EasyMint'){
+
+        MaxSupply = 20;
+    }
+
+    function toggleisMintEnabled() external  onlyOwner() {
+        isMintEnable = !isMintEnable;
+    }
+
+    function SetMaxSupply(uint256 MaxSupply_) external onlyOwner(){
+        MaxSupply = MaxSupply_;
+
+    }
+    
+    function mint() external payable {
+        require(isMintEnable,"mint not enable");
+        require(mintedWallets[msg.sender] <1 , "Mint max per wallet");
+        require(msg.value == mintprice ,"Wrong value");
+        require(MaxSupply > totalsupply , "Sold out");
+        mintedWallets[msg.sender]++;
+        totalsupply++;
+        uint tokenid = totalsupply;
+        _safeMint (msg.sender , tokenid);
+
+    }
+}
